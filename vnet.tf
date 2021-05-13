815cc38b75b96c4230beb31847c7fbb5b561bff0
guptari12 <84148992+guptari12@users.noreply.github.com>
2021-05-13 16:46:05 -0500

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/rishigupta-training/network/azurerm"
   version = "3.0.1"
-  resource_group_name = "rishi-test-rg-001"
+  resource_group_name = "${var.prefix}-workshop"
 }
